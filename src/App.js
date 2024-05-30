75167a233ea0286de4d8201a16be6303f001b5e3
Nailya Shakirova <nailya.shakirova@foam.agency>
2024-05-30 14:06:49 +0200

    Initial app with no API calling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,206 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useReducer } from 'react';
+import { countries, options, questions, scores } from './constants';
+import RadioGroup from '@mui/material/RadioGroup';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from "@mui/material/FormLabel";
+import Radio from '@mui/material/Radio';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import Container from '@mui/material/Container';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [state, setState] = useState({
+    age: '',
+    gender: '',
+    ethnicity: '',
+    country: '',
+    jundice: '',
+    A1: '',
+    A2: '',
+    A3: '',
+    A4: '',
+    A5: '',
+    A6: '',
+    A7: '',
+    A8: '',
+    A9: '',
+    A10: '',
+  });
+  const [, forceUpdate] = useReducer(o => !o);
+
+  let error;
+  let disabled = false;
+
+
+  const handleChange = (event, field) => {
+    const localState = state;
+    localState[field] = event.target ? event.target.value : event;
+    setState(state);
+    forceUpdate();
+  }
+  const processData = (event) => {
+    disabled = true;
+    error = !!Object.keys(state).find(key => !state[key]);
+    if (error) {
+      forceUpdate();
+      console.log(error);
+      event.preventDefault();
+      disabled = false;
+      return;
+    }
+    setResult(Object.keys(state).reduce((acc, key) =>
+      scores[key] ? scores[key] + acc : acc
+      , 0));
+    forceUpdate();
+    event.preventDefault();
+  }
+
+
+  const optionsHtml = options.map((option, i) => <FormControlLabel value={option} control={<Radio />} key={option} label={option} />)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ArrowDownwardIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography>Autism detection</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Autism detection questionnaire.
+             Please note, none of the personal data is gathered.
+             The data entered will be used for federated training.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {!result &&<Box component="form"
+       onSubmit={processData}
+        sx={{ mt: 3 }}
+        >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField label="Age"
+              type="number"
+              name="age"
+              onChange={(event) => handleChange(event, 'age')}
+              required
+              error={error && !state.age}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Ethnicity"
+              type="text"
+              name="ethnicity"
+              onChange={(event) => handleChange(event, 'ethnicity')}
+              required
+              error={error && !state.ethnicity}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <InputLabel id="country-label">Country</InputLabel>
+            <Select required
+              labelId="country-label"
+              value={state.country}
+              error={error && !state.country}
+              onChange={event => handleChange(event, 'country')}
+              label="Country">
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {countries.map(country => <MenuItem value={country} key={country}>{country}</MenuItem>)}
+            </Select>
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel id="gender">Gender</FormLabel>
+            <RadioGroup
+              required
+              defaultValue="Female"
+              name="gender"
+              value={state.gender}
+              error={error && !state.gender}
+              onChange={(event) => handleChange(event.target.value, 'gender')}
+            >
+              <FormControlLabel value="Female" control={<Radio />} label="Female" />
+              <FormControlLabel value="Male" control={<Radio />} label="Male" />
+            </RadioGroup>
+
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel id="jundice">Occurrence of autism in family</FormLabel>
+            <RadioGroup
+              required
+              name="jundice"
+              error={error && !state.jundice}
+              value={state.jundice}
+              onChange={(event) => handleChange(event.target.value, 'jundice')}
+            >
+              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+              <FormControlLabel value="No" control={<Radio />} label="No" />
+            </RadioGroup>
+
+          </Grid>
+          {questions.map((question, i) =>
+            <Grid item key={question.substring(0, 20)} xs={12}>
+              <FormLabel id={question.substring(0, 20)}>{question}</FormLabel>
+              <RadioGroup label={question}
+                required
+                error={error && !state['A' + (i + 1)]}
+                name={'A' + (i + 1)}
+                onChange={(event) => handleChange(event.target.value, 'A' + (i + 1))}
+                value={state['A' + (i + 1)]}>
+                {optionsHtml}
+              </RadioGroup>
+            </Grid>)}
+          <Grid item>
+            <Button type="submit">Submit</Button></Grid>
+        </Grid>
+      </Box>}
+      {
+        result && 
+        <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">
+      
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        The result is
+      </Typography>
+      <Typography variant="h5" component="div">
+        {result >= 6 ? "You likely have autism" : "You likely don't have autism"}
+      </Typography>
+      <Typography variant="body2">
+        Score: {result}
+        <br/>
+        Please consult with a specilist to validate
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" target ="_blank" href="https://thespectrum.org.au/autism-diagnosis/checklist-adults/">Learn More</Button>
+    </CardActions>
+    </Card>
+    </Box>
+      }
+    </Container>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const countries = ['United States', 'Brazil', 'Spain', 'Egypt', 'New Zealand', 'Bahamas',
+    'Burundi', 'Austria', 'Argentina', 'Jordan', 'Ireland', 'United Arab Emirates',
+    'Afghanistan', 'Lebanon', 'United Kingdom', 'South Africa', 'Italy',
+    'Pakistan', 'Bangladesh', 'Chile', 'France', 'China', 'Australia', 'Canada',
+    'Saudi Arabia', 'Netherlands', 'Romania', 'Sweden', 'Tonga', 'Oman', 'India',
+    'Philippines', 'Sri Lanka', 'Sierra Leone', 'Ethiopia', 'Viet Nam', 'Iran',
+    'Costa Rica', 'Germany', 'Mexico', 'Russia', 'Armenia', 'Iceland', 'Nicaragua',
+    'Hong Kong', 'Japan', 'Ukraine', 'Kazakhstan', 'AmericanSamoa', 'Uruguay',
+    'Serbia', 'Portugal', 'Malaysia', 'Ecuador', 'Niger', 'Belgium', 'Bolivia',
+    'Aruba', 'Finland', 'Turkey', 'Nepal', 'Indonesia', 'Angola', 'Azerbaijan',
+    'Iraq', 'Czech Republic', 'Cyprus'].sort();
+
+export const questions = [
+    "I  often  notice  small  sounds  when  others  do not",
+    "I   usually   concentrate   more   on   the   wholepicture, rather than the small details",
+    "I find it easy to do more than one thing at once",
+    "If there is an interruption, I can switch back to what I was doing very quickly",
+    "I find it easy to ‘read between the lines’ whensomeone is talking to me",
+    "I know how to tell if someone listening to me is getting bored",
+    "When I’m reading a story I find it difficult towork out the characters’ intentions",
+    "I like to collect information about categories of things",
+    "I find it easy to work out what someone is thinking or feeling just by looking",
+    "I find it difficult to work out people’s intentions",
+]
+export const scores = { A1: 1, A2: 0, A3: 0, A4: 0, A5: 0, A6: 0, A7: 1, A8: 1, A9: 0, A10: 1 };
+export const options = ['Definitely agree', 'Slightly agree', 'Slightly disagree', 'Definitely disagree']
